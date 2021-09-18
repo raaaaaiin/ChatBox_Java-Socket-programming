2c889786e2d391eb5d0a0e722b93b4b059f21985
raaaaaiin <marcraineer0089@gmail.com>
2021-09-19 06:09:44 +0800

    Initial Commit

---
+++ src/clientservergui/ClientServerGui.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientservergui;
+
+
+
+/**
+ *
+ * @author Gabby
+ */
+public class ClientServerGui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AdminChat _admin = new AdminChat();
+        _admin.show();
+        
+    }
+    
+}
